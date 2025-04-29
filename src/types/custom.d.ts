ef2f62c1fc75a9c9ae32125bd60693a7d0e61d2a
Puru Goyal <puru@Purus-MacBook-Air.local>
2025-04-29 18:52:20 +0530

    fix: custom type declarations for react-icons/fa and third-party modules

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,3 +1,5 @@
 // src/types/custom.d.ts
-// stub out any modules lacking types:
-declare module 'bitcoin-core';
+// for modules with no @types on npm:
+declare module "bcrypt";
+declare module "jsonwebtoken";
+declare module "bitcoin-core";
--- src/types/react-icons-fa.d.ts
+++ src/types/react-icons-fa.d.ts
@@ -2,11 +2,12 @@
 import * as React from "react";
 
 declare module "react-icons/fa" {
+  /** Each Font-Awesome icon is really a React.FC producing an SVG element */
   export const FaWallet: React.FC<React.SVGProps<SVGSVGElement>>;
-  export const FaStar: React.FC<React.SVGProps<SVGSVGElement>>;
-  export const FaCoins: React.FC<React.SVGProps<SVGSVGElement>>;
-  export const FaPercent: React.FC<React.SVGProps<SVGSVGElement>>;
-  export const FaChartLine: React.FC<React.SVGProps<SVGSVGElement>>;
-  export const FaBitcoin: React.FC<React.SVGProps<SVGSVGElement>>;
-  export const FaTrashAlt: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const FaStar:   React.FC<React.SVGProps<SVGSVGElement>>;
+  export const FaCoins:  React.FC<React.SVGProps<SVGSVGElement>>;
+  export const FaPercent:React.FC<React.SVGProps<SVGSVGElement>>;
+  export const FaChartLine:React.FC<React.SVGProps<SVGSVGElement>>;
+  export const FaBitcoin:React.FC<React.SVGProps<SVGSVGElement>>;
+  export const FaTrashAlt:React.FC<React.SVGProps<SVGSVGElement>>;
 }
